d2446f4bf0707db49ec45fcd291c8e90d9c4d6a0
Miguel Silva <miguelafsilva5@gmail.com>
2024-02-16 13:04:21 +0000

    feat: Add some testing asserts
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- tests/src/hello_world_test.c
+++ tests/src/hello_world_test.c
@@ -3,9 +3,11 @@
 BAO_TEST(HELLO, TEST_A)
 {
     printf("Hello World!!!\n");
+    EXPECTED_EQUAL(0,1);
 }
 
 BAO_TEST(HELLO, TEST_B)
 {
+    EXPECTED_EQUAL(0,0);
     printf(" Bao Test Framework is up!!!\n");
 }
